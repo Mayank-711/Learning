d9ce82d5d102e8aabf416cce77849b92dbe67de3
MAYANK MISHRA <mayank71104@gmail.com>
2023-12-26 22:33:56 +0530

    Baic OOps

---
+++ Practice Python/harry and gfg/setmethods.py
@@ -0,0 +1,5 @@
+a ={1,2,3,4}
+b={2,4,6}
+print(a.union(b))
+print((a.intersection(b)))
+print(a.difference(b))
--- Practice Python/mosh/exception.py
+++ Practice Python/mosh/exception.py
@@ -2,6 +2,8 @@ try:
     a = int(input("Enter a number:"))
     b = int(input("Enter another number:"))
     c = a/b
+    if b == 0:
+        raise ValueError('Dont Enter 0')
 except ValueError:
     print('Pls enter a number')
 except ZeroDivisionError:
