1930d29383dda0023325ce9056c74d7f2e27c4d5
MAYANK MISHRA <mayank71104@gmail.com>
2024-10-11 20:24:52 +0530

    part1

---
+++ React/app1/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Kilua Coder</h1>
+  );
+}
+
+export default App;
---
+++ React/vite1/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <h1> Killua Coder </h1>
+  )
+}
+
+export default App
