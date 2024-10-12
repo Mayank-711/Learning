fee5ff552676ddddf5e0560c4fc39a8151a93c3b
MAYANK MISHRA <mayank71104@gmail.com>
2024-10-12 12:18:31 +0530

    005

--- React/app1/src/App.js
+++ React/app1/src/App.js
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <h1>Kilua Coder</h1>
+    <><h1>Kilua Coder</h1><ul>
+      <li>Hunter X Hunter</li>
+      <li>Naruto</li>
+      <li>One Piece</li>
+    </ul></>
   );
 }
 
